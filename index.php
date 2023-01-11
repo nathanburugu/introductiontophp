3feaf7962a004432f142eac85315f820c6626df0
ybnate254 <95563035+ybnate254@users.noreply.github.com>
2023-01-11 22:33:26 +0300

    php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!-- @format -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <?php
+    $num1 = 10;
+    $num2 = 15;
+    $result = $num1 + $num2;
+    echo "The Answer is ".$result;
+    $name = "nate";
+    echo strtoupper($name) ;
+    echo str_replace('n', 'l', $name);
+    echo strlen($name);
+    $num1 = 10;
+    $num1 ++;
+    echo $num1 ;
+
+    ?>
+  </body>
+</html>
